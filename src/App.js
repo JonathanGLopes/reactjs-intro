cb93205cb05330552625e27921bfa319efee3268
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-11-18 18:51:59 -0300

    aplicando listas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
-import Membro from './components/Membro';
+import Feed from './components/feed';
 
 class App extends Component{
 
 
+    constructor(props){
+        super(props);
+        this.state = {
+            feed: [ 
+                {id: 1, username: 'Jonathan', curtidas: 10, comentarios: 2},
+                {id: 2, username: 'Matheus', curtidas: 32, comentarios: 23},
+                {id: 3, username: 'Amanda', curtidas: 54, comentarios: 41},
+            ]
+        };
+    }
+
     render(){
         return(
             <div>
-               <Membro nome="Visitante"/>
+               {this.state.feed.map((item) => {
+                return(
+                    <Feed id={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios}/>
+                );
+               })}
             </div>
         )
     }
