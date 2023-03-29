9b927b164480020dc2c0af8734c6d01903d26334
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-03-29 18:27:48 -0300

    Link do sobre

--- src/App.js
+++ src/App.js
@@ -1,59 +1,12 @@
-import React, {Component} from 'react';
+import React from "react";
+import './ITCSS/generic/reset.css';
+import './ITCSS/settings/colors.css'
+import './index.css';
 
-class App extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            form: {
-                nome: '',
-                email: '',
-                senha: '',
-                sexo: ''
-            }
-        };
-
-        this.dadosForm = this.dadosForm.bind(this);
-    }
-
-    dadosForm(e){
-        let form = this.state.form;
-        form[e.target.name] = e.target.value;
-        this.setState({form: form});
-    }
-
-
-    render(){
-        return(
-            <div>
-                <h1> Login </h1>
-                Nome:
-                <input type="text" name="nome" value={this.state.form.nome} onChange={this.dadosForm}></input> <br/><br/>
-
-                Email:
-                <input type="email" name="email" value={this.state.form.email} onChange={this.dadosForm}></input> <br/><br/>
-
-                Senha:
-                <input type="text" name="senha" value={this.state.form.senha} onChange={this.dadosForm}></input><br/><br/>
-
-                Sexo:
-                <select name='sexo' value={this.state.form.sexo} onChange={this.dadosForm}>
-                    <option value="masculino"> Masculino </option>
-                    <option value="feminino"> Feminino </option>
-                </select>
-
-                <div>
-                    <h3>{this.state.form.email}</h3>
-                    <h3>{this.state.form.senha}</h3>
-                    <h3>{this.state.form.sexo}</h3>
-                </div>
-            </div>
-            
-        )
-    }
-
-
+function App(){
+    return(
+        <a href="to-do" className="action">Sobre</a>
+    )
 }
-
 
 export default App;
