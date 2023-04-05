09925e20573dc5c87977f454d9be58058461b3fa
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-04-05 16:23:55 -0300

    Criado o primeiro object

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './styles/settings/colors.css';
 import './styles/generic/reset.css';
 import './styles/elements/base.css';
-import './App.css';
 import App from './App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
