a46a2ee8baa20e9344a2aea40dfaf0e65b762620
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-04-05 18:06:09 -0300

    Criamos o nosso logo e importamos ele no App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Sobre from './objects/Sobre';
+import Logo from './objects/Logo';
 
 function App(){
     return(
-        <Sobre />
+        // A abertura e fechamento envolvendo os elementos se trata de uma abreviação do Fragment
+        // para evitar a criação de divs desnecessárias e melhorar o nosso código.
+        <>
+            <Logo />
+            <Sobre />
+        </>
+
     );
 }
 
---
+++ src/objects/Logo/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CollabCode from '../../img/logo.png';
+
+const Logo = () => {
+    return (
+        <img src={CollabCode} alt="Logo da CollabCode" className="logo"/>
+    )
+}
+
+export default Logo;
